94079e1cd6311a038708acd3393e7761ec634867
Dmitrii Pikulin <aspirisen@gmail.com>
2020-07-20 17:27:35 +0300

    tt22

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import {App} from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 console.log(1);
-console.log(7);
+console.log(2);
 console.log(6);
 console.log(7);
 console.log(6);
